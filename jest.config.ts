4c4a088a163df9e3a33308f1a560d99eb33ecd78
Arnaud <arnaud@dauran.com>
2023-01-29 17:51:15 +0100

    fix: make unit tests work with esm

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
     roots: ['<rootDir>'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
     },
     testMatch: ['<rootDir>/src/**/*.test.ts'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
